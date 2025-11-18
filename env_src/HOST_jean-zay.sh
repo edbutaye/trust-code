7b2c6e33ae5a18e745cc4dfd2580472e4dcce14f
Pierre LEDAC <pierre.ledac@cea.fr>
2025-11-18 11:53:11 +0100

    [GPU] Update JeanZay conf + bench H100 results

--- env_src/HOST_jean-zay.sh
+++ env_src/HOST_jean-zay.sh
@@ -16,18 +16,11 @@ define_modules_config()
    # Load modules
    if [ "$TRUST_USE_CUDA" = 1 ]
    then
-      module="gcc/11.4.1 cmake/3.31.4 "
-      # On utilise desormais le CUDA de NVHPC:
-      if [ "$TRUST_FORCE_MPI_GPU_AWARE" = 1 ]
-      then
-         # MPI GPU-Aware fait diverger plusieurs preconditionneurs PETSc 3.20 dont boomeramg le plus performant... Support contacte
-         module=$module" openmpi/4.1.5-cuda"
-      else
-         module=$module" openmpi/4.1.5" # On telecharge le NVHPC 23.5 pour pouvoir compiler Kokkos 4.5 (23.1 trop vieux)
-      fi
-      [ "$TRUST_CUDA_CC" = "" ] && TRUST_CUDA_CC=70           # V100
-      [ "$TRUST_CUDA_CC" = 80 ] && module="arch/a100 "$module # A100
-      [ "$TRUST_CUDA_CC" = 90 ] && module="arch/h100 gcc/11.3.1 openmpi/4.1.5-cuda" # H100 (pas le choix sur gcc et openmpi...)
+      [ "$TRUST_CUDA_CC" = "" ] && TRUST_CUDA_CC=70
+      [ "$TRUST_CUDA_CC" = 70 ] && module="gcc/11.4.1"           # V100
+      [ "$TRUST_CUDA_CC" = 80 ] && module="arch/a100 gcc/11.4.1" # A100
+      [ "$TRUST_CUDA_CC" = 90 ] && module="arch/h100 gcc/11.3.1" # H100
+      module=$module" openmpi/4.1.5-cuda cmake/3.31.4 python/3.11.5"
    else
       # 21/06/2023 : ND : passage a gcc
       module="gcc/8.5.0 openmpi/4.0.5"
@@ -85,7 +78,7 @@ define_soumission_batch()
       # See http://www.idris.fr/jean-zay/gpu/jean-zay-gpu-exec_partition_slurm.html#les_qos_disponibles
       q="" && [ $arch != v100 ] && q="_"$arch
       qos=qos_gpu$q-t3 && cpu=1200 && [ "$prod" != 1 ] && [ $NB_PROCS -le 32 ] && qos=qos_gpu$q-dev && cpu=120 
-      for project in cpx aih hbx # Hackathon GENDEN eDARI 
+      for project in aih # GENDEN
       do
 	 [ "`id | grep $project`" != "" ] && project="$project@$arch" && break
       done
